8cb5334ed6180a6d5959f4f118c2e2d724f12778
AYOPOSI O <aolu-bamisaye@loyola.edu>
2025-07-28 02:04:32 -0400

    force favicon cache bust with version parameter

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,11 +50,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Bitter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/Logo.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/Logo.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/Logo.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/Logo.png?v=2" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/Logo.png?v=2" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/Logo.png?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={`${fjallaOne.variable} ${bitter.variable} font-bitter`}>
